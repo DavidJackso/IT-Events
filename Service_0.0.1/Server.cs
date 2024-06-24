95af2e1ea7510710313a6239777421cd91482e84
Joseph <gutnov3643@gmail.com>
2024-06-24 22:43:37 +0300

    Добавление работы с сервером

---
+++ Service_0.0.1/Server.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+using Renci.SshNet.Security;
+
+namespace Service
+{
+    internal class Server
+    {
+        static string pathtokey = "key.pub";
+        static FileStream stream = new FileStream(pathtokey, FileMode.Open, FileAccess.Read);
+        static PrivateKeyFile key = new PrivateKeyFile(stream);
+        static ConnectionInfo connectionInfo = new ConnectionInfo("51.250.17.119","joseph",new PrivateKeyAuthenticationMethod("joseph", key));
+        static string[] bases = new string[]{"users.json","userstoevents.json","events.json"};
+        public Server()
+        {
+
+        }
+        static public void DownloadBases()
+        {
+            try
+            {
+                var client = new SftpClient(connectionInfo);
+                client.Connect();
+                foreach (var item in bases)
+                {
+                    Stream fileStream = File.Create(item);
+                    client.DownloadFile(item, fileStream);
+                    fileStream.Close();
+                }
+                client.Disconnect();
+            }
+            catch (Renci.SshNet.Common.SshAuthenticationException)
+            {
+                MessageBox.Show("Не удаеться получить доступ к серверу.Проверьте подключение к интернету и обратитесь в службу тех поддержки");
+            }
+        }
+        static public void UpdateBases()
+        {
+            try
+            {            
+                var client = new SftpClient(connectionInfo);
+                client.Connect();
+                foreach (var item in bases)
+                {
+                    Stream fileStream = File.Create(item);
+                    client.UploadFile(fileStream, item);
+                    fileStream.Close();
+                }
+                client.Disconnect(); 
+            }
+            catch (Renci.SshNet.Common.SshAuthenticationException)
+            {
+                MessageBox.Show("Не удаеться получить доступ к серверу.Проверьте подключение к интернету и обратитесь в службу тех поддержки");
+            }
+        }
+    }
+}
